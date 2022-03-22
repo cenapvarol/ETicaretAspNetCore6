a977b7984d50a62a505fab2caa4fea2e6bf2f46c
cenap varol <cenap.varol@ibb.gov.tr>
2022-03-22 16:30:04 +0300

    Repository

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Migrations/20220322100842_mig_1.Designer.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Migrations/20220322100842_mig_1.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ETicaretAPI.Persistence.Migrations
 {
     [DbContext(typeof(ETicaretAPIDbContext))]
-    [Migration("20220318222427_mig_1")]
+    [Migration("20220322100842_mig_1")]
     partial class mig_1
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
